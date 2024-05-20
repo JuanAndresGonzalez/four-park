a5a75728fa513fa878b9e7aef813c76259a6091b
juan andres <jandresggonzalez@gmail.com>
2024-05-20 12:50:12 -0500

    correcion de ruta

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
       <Route path="/" element={<EmployeeRegistration />} />
-      <Route path="/inise" element={<Inise />} />{/*prueba de conexion nueva2.0*/}
+      <Route path="/inise" element={<Inise />} />
       <Route path="/agregarParqueadero" element={<AgregarParqueadero />} />
     </Routes>
   </Router>
