8c0ea76f52466638f2c01298dffd4ca0b7b89b47
juan andres <jandresggonzalez@gmail.com>
2024-05-26 12:29:06 -0500

    Se modifica vista gerente

--- src/gerente.jsx
+++ src/gerente.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Gerente() {
+  const navigate = useNavigate();
+
+  const handleBackingClick = () => {
+    navigate('/'); // Ruta a la vista Home
+  };
+
   return (
     <div>
       <h1>Panel del Gerente</h1>
-        <a href="/agregarParqueadero">Agregar un nuevo parqueadero</a><br />
-        <a href="/emploRegister">Registrar Empleado</a><br />
-        <a href="/ver-informe">Ver Informe</a>
+      <a href="/agregarParqueadero">Agregar un nuevo parqueadero</a><br />
+      <a href="/emploRegister">Registrar Empleado</a><br />
+      <a href="/ver-informe">Ver Informe</a>
+      <p><button type="button" onClick={handleBackingClick}>Salir</button></p>
     </div>
   );
 }
+
 export default Gerente;
