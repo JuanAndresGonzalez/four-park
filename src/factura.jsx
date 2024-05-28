60555281b71f25b9d884fb7c43cfdcd8e95240d4
Felipe Abril <felipeabril520@gmail.com>
2024-05-28 00:24:08 -0500

    Intento actualizacion estetica vista factura

--- src/factura.jsx
+++ src/factura.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
-
 import { useNavigate } from 'react-router-dom';
 import './Factura.css';
 
 const Factura = () => {
-    const navigate = useNavigate(); // Get useNavigate hook instance
-}
+  const navigate = useNavigate(); // Get useNavigate hook instance
 
-const factura = () => {
   const [form, setForm] = useState({
     email: '',
     idReserva: '',
@@ -32,43 +29,48 @@ const factura = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="AddParking">
+      <div className="formcontainer">
         <h1>Factura</h1>
-      <label>
-        Email:
-        <input type="email" name="email" value={form.email} onChange={handleChange} required />
-      </label><br />
-      <label>
-        ID Reserva:
-        <input type="number" name="idReserva" value={form.idReserva} onChange={handleChange} required />
-      </label><br />
-      <label>
-        ID Factura:
-        <input type="number" name="idFactura" value={form.idFactura} onChange={handleChange} required />
-      </label><br />
-      <label>
-        Hora Inicio:
-        <input type="time" name="horaInicio" value={form.horaInicio} onChange={handleChange} required />
-      </label><br />
-      <label>
-        Hora Fin:
-        <input type="time" name="horaFin" value={form.horaFin} onChange={handleChange} required />
-      </label><br />
-      <label>
-        Fecha Inicio:
-        <input type="date" name="fechaInicio" value={form.fechaInicio} onChange={handleChange} required />
-      </label><br />
-      <label>
-        Fecha Fin:
-        <input type="date" name="fechaFin" value={form.fechaFin} onChange={handleChange} required />
-      </label><br />
-      <label>
-        Precio:
-        <input type="number" name="precio" value={form.precio} onChange={handleChange} required />
-      </label><br />
-      <button type="submit">Imprimir Factura</button>
-    </form>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="email" name="email" value={form.email} onChange={handleChange} required />
+          </label><br />
+          <label>
+            ID Reserva:
+            <input type="number" name="idReserva" value={form.idReserva} onChange={handleChange} required />
+          </label><br />
+          <label>
+            ID Factura:
+            <input type="number" name="idFactura" value={form.idFactura} onChange={handleChange} required />
+          </label><br />
+          <label>
+            Hora Inicio:
+            <input type="time" name="horaInicio" value={form.horaInicio} onChange={handleChange} required />
+          </label><br />
+          <label>
+            Hora Fin:
+            <input type="time" name="horaFin" value={form.horaFin} onChange={handleChange} required />
+          </label><br />
+          <label>
+            Fecha Inicio:
+            <input type="date" name="fechaInicio" value={form.fechaInicio} onChange={handleChange} required />
+          </label><br />
+          <label>
+            Fecha Fin:
+            <input type="date" name="fechaFin" value={form.fechaFin} onChange={handleChange} required />
+          </label><br />
+          <label>
+            Precio:
+            <input type="number" name="precio" value={form.precio} onChange={handleChange} required />
+          </label><br />
+          <button type="submit" className="actionButton">Imprimir Factura</button>
+        </form>
+        <button className="exitButton" onClick={() => navigate('/')}>Salir</button>
+      </div>
+    </div>
   );
 };
 
-export default factura;
+export default Factura;
