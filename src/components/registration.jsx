ff075e1bdc82e79409e9945e6f4b5fcb106c3c53
Valentina <valentivelasquezm@gmail.com>
2024-05-29 21:11:13 -0500

    integrado funcionalidad de registrarse con backend

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import logo from "../assets/img/logo.png";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/Registration.module.css";
+import { client } from "../services/apirest";
 
 const Registration = () => {
   const navigate = useNavigate();
+  const [correo, setCorreo] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [contrasena, setContrasena] = useState("");
 
-  const handleLoginClick = () => {
-    navigate("/inise");
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      correo_electronico: correo,
+      nombre: nombre,
+      apellido: apellido,
+      contrasena: contrasena,
+      id_rol: "2    ",
+    };
+
+    try {
+      const response = await client.post("/api/usuarios", user);
+
+      if (response.status === 201) {
+        console.log("Usuario registrado:", response.data);
+        navigate("/inise");
+      } else {
+        console.error("Error:", response.data.message);
+        alert(response.data.message);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("Error de registro:", error.response.data.message);
+        alert(error.response.data.message);
+      } else {
+        console.error("Error:", error.message);
+      }
+    }
   };
+
   return (
     <div className={styles.registration}>
-      <div className={styles.logo - container}>
+      <div className={styles.logocontainer}>
         <img src={logo} alt="Four Parking logo" className="logo" />
       </div>
-      <div className={styles.form - container}>
-        <div className={styles.form - content}>
+      <div className={styles.formcontainer}>
+        <div className={styles.formcontent}>
           <h2>Regístrate</h2>
-          <form>
+          <form onSubmit={handleRegister}>
             <input
               type="email"
               placeholder="Correo Electrónico"
               required
+              value={correo}
+              onChange={(e) => setCorreo(e.target.value)}
               className="input-field"
             />
             <br />
@@ -29,6 +65,8 @@ const Registration = () => {
               type="text"
               placeholder="Nombre"
               required
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
               className="input-field"
             />
             <br />
@@ -36,21 +74,33 @@ const Registration = () => {
               type="text"
               placeholder="Apellido"
               required
+              value={apellido}
+              onChange={(e) => setApellido(e.target.value)}
               className="input-field"
             />
             <br />
-            <input type="password" placeholder="Contraseña" required />
-            <br className="input-field" />
+            <input
+              type="password"
+              placeholder="Contraseña"
+              required
+              value={contrasena}
+              onChange={(e) => setContrasena(e.target.value)}
+              className="input-field"
+            />
             <br />
-            <button type="submit" className={styles.registerF - button}>
+            <button
+              type="submit"
+              className={styles.registerFbutton}
+              onClick={handleRegister}
+            >
               Regístrate
             </button>
           </form>
           <p>
             ¿Ya tienes una cuenta?{" "}
             <button
-              onClick={handleLoginClick}
-              className={styles.rout - buttonER}
+              onClick={() => navigate("/inise")}
+              className={styles.routbuttonER}
             >
               Inicia sesión
             </button>
@@ -59,10 +109,6 @@ const Registration = () => {
       </div>
     </div>
   );
-};
-
-export const handleLoginClick = (navigate) => {
-  navigate("/inise");
 };
 
 export default Registration;
